b8bf43bf3ef0803306ca34fff7d71017448a0a05
Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2023-09-20 14:42:41 -0500

    folder rename

--- Data Analysis with Python Projects/Sea Level Predictor/sea_level_predictor.py
+++ Data Analysis with Python Projects/Sea Level Predictor/sea_level_predictor.py
@@ -4,12 +4,18 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv(r'C:\Users\tdeyo\Desktop\Code\FreeCodeCamp\Data Analysis with Python Projects\Sea Level Predictor\sea_level_predictor.py')
 
     # Create scatter plot
+    plt.scatter(df)
 
+    # Add labels and a title
+    plt.xlabel('Year')
+    plt.ylabel('CSIRO Adjusted Sea Level')
+    plt.title('Scatter Plot Example')
 
     # Create first line of best fit
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
 
 
     # Create second line of best fit
