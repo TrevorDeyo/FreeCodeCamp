aaee3716bcd5fcd0b104ee2b78884c8d1b9dba4f
Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2023-09-21 14:14:09 -0500

    data analysis done

--- Data Analysis with Python Projects/Sea Level Predictor/sea_level_predictor.py
+++ Data Analysis with Python Projects/Sea Level Predictor/sea_level_predictor.py
@@ -11,46 +11,29 @@ def draw_plot():
     sea_level = df['CSIRO Adjusted Sea Level']
 
     # Create scatter plot
+    fig, ax = plt.subplots()
     plt.scatter(year, sea_level)
-    plt.xlabel('Year')
-    plt.ylabel('Sea Level (inches)')
-    plt.title('Rise in Sea Level')
 
-    # Linear Regression For all data
-    slope, intercept, _, _, _ = linregress(year, sea_level)
+    # Create first line of best fit
+    res = linregress(year, sea_level)
+    year_prediction = pd.Series([i for i in range(1880,2051)])
+    sea_level_prediction = res.slope * year_prediction + res.intercept
+    plt.plot(year_prediction, sea_level_prediction, "r")
 
-    # regression line equation for all data
-    regression_line = slope * year + intercept
+    # Create second line of best fit
+    new_df = df.loc[df['Year'] >= 2000]
+    future_years_prediction = new_df['Year']
+    future_sea_level_prediction = new_df["CSIRO Adjusted Sea Level"]
+    res_2 = linregress(future_years_prediction, future_sea_level_prediction)
+    x_pred2 = pd.Series([i for i in range(2000,2051)])
+    y_pred2 = res_2.slope * x_pred2 + res_2.intercept
+    plt.plot(x_pred2, y_pred2, 'green')
 
-    # Extend regression line to 2050
-    future_years = list(range(1880, 2051))
-    future_sea_levels = [slope * year + intercept for year in future_years]
+    # Add labels and title
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sea Level (inches)')
+    ax.set_title('Rise in Sea Level')
 
-    # Plot regression line for all data (historical data)
-    plt.plot(year, regression_line, color='red', label='Linear Regression (Historical Data)')
-
-    # plot extended regression line (future prediction)
-    plt.plot(future_years, future_sea_levels, linestyle='--', color='green', label='Future Prediction (2050)')
-
-    #linear Regression for data from year 2000 onwards
-    recent_data = df[df['Year'] >= 2000]
-    recent_year = recent_data['Year']
-    recent_sea_level = recent_data['CSIRO Adjusted Sea Level']
-
-    slope_recent, intercept_recent, _, _, _ = linregress(recent_year, recent_sea_level)
-
-    # Regression line equation for data from year 2000 onwardsk
-    regression_line_recent = slope_recent * year + intercept_recent
-
-    # Extend regression line to 2050 for data from year 2000 onwards
-    future_sea_levels_recent = [slope_recent * year + intercept_recent for year in future_years]
-
-    # Plot regression line for data from year 2000 onwards (historical data)
-    plt.plot(year, regression_line_recent, color='blue', label='Linear Regression (Since 2000)')
-
-    # Plot extended regression line for data from year 2000 onwards (future prediction)
-    plt.plot(future_years, future_sea_levels_recent, linestyle='--', color='purple', label='Future Prediction (Since 2000)')
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
